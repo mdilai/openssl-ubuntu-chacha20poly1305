6a8cffbc309a8c0a13c47354f191dca38662bd2b
Maksym Dilai <admin@dproject.org.ua>
2016-09-01 17:37:31 +0300

    patch.sh wrapper v2

--- patch.sh
+++ patch.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 for i in `cat debian/patches/series`
 do
-echo Patching {i} \n
+printf "Patching {$i} \n"
 patch -p1 < debian/patches/$i
+read  -n 1 -p "Continue?: " mainmenuinput
 echo \n
 done
