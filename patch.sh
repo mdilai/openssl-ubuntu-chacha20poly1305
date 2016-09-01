cb90190d29e4fbcc34fae48b86a12f5ad0627830
Maksym Dilai <admin@dproject.org.ua>
2016-09-01 18:02:52 +0300

    v3

--- patch.sh
+++ patch.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 for i in `cat debian/patches/series`
 do
-printf "Patching {$i} \n"
-patch -p1 < debian/patches/$i
+printf "Patching ${i} \n"
+patch -p1 < debian/patches/${i}
 read  -n 1 -p "Continue?: " mainmenuinput
 echo \n
 done
