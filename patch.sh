5da7dac48d0e4351c742851f49776b2bd21e2457
Maksym Dilai <admin@dproject.org.ua>
2016-09-01 18:46:41 +0300

    patch.sh

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 for i in `cat series`
 do
 printf "Patching ${i} \n"
-patch -p1 < debian/patches/${i}
-read  -n 1 -p "Continue?: " mainmenuinput
+patch -p1 --dry-run < debian/patches/${i}
+#read  -n 1 -p "Continue?: " mainmenuinput
 echo \n
 done
